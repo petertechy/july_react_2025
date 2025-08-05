909e7c769cea6fbeb17bb1e631b7a9a5f5f5f69a
petertechy <petertechy01@gmail.com>
2025-08-05 22:13:41 +0100

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+  const [number, setnumber] = useState(8)
+  const myName = "Kenny"
+  let myNumber = 9
+  const addNumber = ()=>{
+    // myNumber += 1
+    // myNumber = myNumber + 1
+    // myNumber++
+    // console.log(myNumber)
+    setnumber(number + 1)
+    console.log(number)
+  }
   return (
-    <div>
-        <h1>Contact Page</h1>
-    </div>
+    <>
+      <div className='border col-7 mx-auto p-3 my-4 text-center'>
+        <h1>{myName}</h1>
+        <h1>{number}</h1>
+        <button onClick={addNumber} className='btn btn-lg btn-danger'>Increase Number</button>
+      </div>
+       
+       
+        
+    </>
   )
 }
 
