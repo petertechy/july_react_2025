9ea71203a9028bf776dbee64b7a3acf427be5c73
petertechy <petertechy01@gmail.com>
2025-08-06 23:08:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import Homepage from './pages/Homepage'
-import AboutPage from './pages/AboutPage'
-import Contact from './pages/Contact'
-import NotFound from './pages/NotFound'
-import UserProfile from './pages/UserProfile'
-import FormData from './pages/FormData'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import AboutPage from "./pages/AboutPage";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import UserProfile from "./pages/UserProfile";
+import FormData from "./pages/FormData";
+import Fish from "./pages/Fish";
+import Meat from "./pages/Meat";
+import Product from "./pages/Product";
+import FetchData from "./pages/FetchData";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Homepage/>}/>
-        <Route path="home" element={<Navigate to="/" />}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="home" element={<Navigate to="/" />} />
+
+        <Route path="about" element={<AboutPage />} />
+        <Route path="aboutus" element={<Navigate to="about" />} />
+
+        <Route path="contact" element={<Contact />} />
+
+        <Route path="form" element={<FormData />} />
+
+        <Route path="profile/:id" element={<UserProfile />} />
+        <Route path="fetch" element={<FetchData />} />
 
 
-        <Route path='about' element={<AboutPage/>}/>
-        <Route path="aboutus" element={<Navigate to="about" />}/>
+        <Route path="product" element={<Product />}>
+          <Route path="fish" element={<Fish />} />
+          <Route path="meat" element={<Meat />} />
+        </Route>
 
-        <Route path='contact' element={<Contact/>}/>
-
-        <Route path='form' element={<FormData/>}/>
-
-        <Route path='profile/:id' element={<UserProfile/>}/>
-
-
-
-
-        <Route path='*' element={<NotFound/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
