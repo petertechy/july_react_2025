4759f25743887ef692fb859b712da258c1ec99db
petertechy <petertechy01@gmail.com>
2025-08-18 15:53:02 +0100

    updated user

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -26,7 +26,7 @@ const Homepage = () => {
         <h1 className="text-primary">Home page</h1>
         <img src={Image1} alt="" width={500} />
         <button className="btn btn-primary w-100 my-2" onClick={handleSubmit}>
-          Submit
+          Submit User
         </button>
       </div>
     </>
